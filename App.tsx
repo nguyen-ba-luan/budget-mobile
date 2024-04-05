a1f1af696e91308e57d80689d428ee382c7a2f48
Nguyen Ba Luan <nbluan@enjoyworks.co.kr>
2024-04-05 16:37:32 +0700

    feat: add supabase

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {
   SafeAreaView,
   StatusBar,
@@ -7,6 +7,14 @@ import {
   ViewStyle,
 } from 'react-native';
 import AppNavigation from './src/navigation';
+import {createClient} from '@supabase/supabase-js';
+import 'react-native-url-polyfill/auto';
+
+const supabaseUrl = 'https://teplpgfdefrpawmktqxa.supabase.co';
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRlcGxwZ2ZkZWZycGF3bWt0cXhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIzMDMwMDQsImV4cCI6MjAyNzg3OTAwNH0.3zjAz5rXijhWxe0GXJ6watV4C6VeRpjTRVNTjoksSJg';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -15,6 +23,17 @@ function App(): React.JSX.Element {
     backgroundColor: '#ffffff',
     flex: 1,
   };
+
+  useEffect(() => {
+    (async () => {
+      const {data, error} = await supabase.from('currencies').insert({
+        id: 2,
+        symbol: '$',
+        name: 'USD',
+      });
+      console.log({data, error});
+    })();
+  }, []);
 
   return (
     <SafeAreaView style={backgroundStyle}>
